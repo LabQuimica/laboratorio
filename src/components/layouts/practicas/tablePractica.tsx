98b1f12de3568a32165e9198bde53b20ef4f7078
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2025-02-08 00:28:28 -0600

    fixing errors

--- src/components/layouts/practicas/tablePractica.tsx
+++ src/components/layouts/practicas/tablePractica.tsx
@@ -1,25 +1,28 @@
 "use client";
-import { useState } from 'react';
+import { useState } from "react";
 import { usePracticas } from "@/hooks/Practicas/usePractica";
 import { docentecolumns, asignadascolumns } from "./columns";
 import DocenteSelector from "./docenteSelector";
 import { Table } from "@/components/table/Table";
 
 interface PracticasTableProps {
-    viewType: 'creadas' | 'asignadas';
-}  
+  viewType: "creadas" | "asignadas";
+}
 
 export const PracticasTable = ({ viewType }: PracticasTableProps) => {
   const { practicasData } = usePracticas();
 
-  const columns = viewType === 'creadas' ? docentecolumns : asignadascolumns;
+  const columns = viewType === "creadas" ? docentecolumns : asignadascolumns;
 
   const filteredPracticas =
     viewType === "creadas"
       ? practicasData.data || []
       : practicasData.data?.filter((practica) => practica.esta_asignada) || [];
 
-  const [selectedDocente, setSelectedDocente] = useState<{ id: number, name: string } | null>(null);
+  const [selectedDocente, setSelectedDocente] = useState<{
+    id: number;
+    name: string;
+  } | null>(null);
   const handleDocenteSelect = (id: number, name: string) => {
     setSelectedDocente({ id, name });
   };
@@ -27,17 +30,18 @@ export const PracticasTable = ({ viewType }: PracticasTableProps) => {
   return (
     <div>
       {/* Selector de docente */}
-      {viewType === "creadas" && <DocenteSelector onDocenteSelect={handleDocenteSelect} />}
+      {viewType === "creadas" && (
+        <DocenteSelector onDocenteSelect={handleDocenteSelect} />
+      )}
 
       {/* Tabla */}
       <Table
-      data={filteredPracticas}
-      columns={columns}
-      isLoading={practicasData.isLoading}
-      isError={practicasData.isError}
-      orderBy="id_vale"
+        data={filteredPracticas}
+        columns={columns}
+        isLoading={practicasData.isLoading}
+        isError={practicasData.isError}
+        orderBy="id_vale"
       />
     </div>
-    
   );
 };
--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -35,9 +35,9 @@ export const links = [
     href: "/menu/practica",
     icon: (
       <IconFileAnalytics className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-   ),
+    ),
   },
-        
+  {
     label: "Usuarios generales",
     href: "/menu/users",
     icon: (
