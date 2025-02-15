16f2993bb9cca322a84e5fa39b2deb49b6e20ec6
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2025-02-14 18:22:04 -0600

    my bad

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -13,11 +13,14 @@ function Menu() {
         alt="logo"
         style={{ width: "400px", height: "400px" }}
       />
-      <h1 style={{ fontSize: "38px", marginTop: "30px" }} className="text-center font-bold">
+      <h1
+        style={{ fontSize: "38px", marginTop: "30px" }}
+        className="text-center font-bold"
+      >
         Hola {user?.name}!
       </h1>
     </div>
   );
 }
 
-export default welcome;
+export default Menu;
