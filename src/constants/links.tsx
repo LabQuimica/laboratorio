6d667c40216843a651c9e96450c0b4803f0c1294
MikeszIPN <msanchezz2100@alumno.ipn.mx>
2025-03-18 15:29:54 -0600

    Fix typo in "Manuañes" label and clean up userService code by removing commented-out functions

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -32,7 +32,7 @@ export const links = [
     ),
   },
   {
-    label: "Manuañes",
+    label: "Manuales",
     href: "/menu/manuales",
     icon: (
       <IconBook className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
