0c22c23d7d5a11a4f823e105bbd5cbf6659bae2b
MikeszIPN <msanchezz2100@alumno.ipn.mx>
2025-03-18 14:19:15 -0600

    Refactor links: remove "Vales" entry and update "Usuarios" label to "Usuarios registrados"

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -25,18 +25,7 @@ export const links = [
     ),
   },
   {
-<<<<<<< HEAD
-    label: "Vales",
-    href: "/menu/vale",
-    icon: (
-      <IconChecklist className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-    ),
-  },
-  {
-    label: "Usuarios",
-=======
     label: "Usuarios registrados",
->>>>>>> origin/master
     href: "/menu/users",
     icon: (
       <IconUsers className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
