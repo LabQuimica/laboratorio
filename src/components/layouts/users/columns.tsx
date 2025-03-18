6425d9216ee23171113e4ab5de06ee00eeb56fae
MikeszIPN <msanchezz2100@alumno.ipn.mx>
2025-03-18 14:07:26 -0600

    Remove Dashboard component and update user columns and links

--- src/components/layouts/users/columns.tsx
+++ src/components/layouts/users/columns.tsx
@@ -7,31 +7,63 @@ import { ColumnDef } from "@tanstack/react-table";
 import formatDateCell from "@/components/table/FormatedDate";
 import PasswordCell from "@/components/layouts/users/showPassword";
 
+// export const columns: ColumnDef<User>[] = [
+//   {
+//     accessorKey: "id_user",
+//     header: ({ column }) => (
+//       <SortableHeader column={column} title="Id" className="justify-center" />
+//     ),
+//     size: 10,
+//     cell: ({ row }) => (
+//       <p className="text-center"> {row.getValue("id_user")} </p>
+//     ),
+//   },
+//   {
+//     accessorKey: "name",
+//     header: ({ column }) => <SortableHeader column={column} title="Nombre" />,
+//     size: 50,
+//   },
+//   {
+//     accessorKey: "email",
+//     header: "Correo electrónico",
+//     size: 50,
+//     cell: ({ row }) => <TruncatedCell text={row.getValue("email")} />,
+//   },
+//   {
+//     accessorKey: "password",
+//     header: "Contraseña",
+//     size: 30,
+//     cell: ({ row }) => <PasswordCell password={row.getValue("password")} />,
+//   },
+//   {
+//     accessorKey: "date",
+//     header: "Fecha",
+//     cell: formatDateCell("date"),
+//   },
+//   {
+//     accessorKey: "rol",
+//     header: "Rol",
+//     size: 30,
+//   },
+//   {
+//     accessorKey: "active",
+//     header: "Estado",
+//     size: 10,
+//     cell: ({ row }) => {
+//       const value = row.getValue("active"); 
+//       return <p className="text-center">{value === 1 ? "Activo" : "No activo"}</p>;
+//     },
+//   },
+  
+//   {
+//     accessorKey: "actions",
+//     header: "Actions",
+//     size: 10,
+//     cell: ({ row }) => <ActionUser row={row} />,
+//   },
+// ];
+
 export const columns: ColumnDef<User>[] = [
-  // {
-  //   id: "select",
-  //   header: ({ table }) => (
-  //     <Checkbox
-  //       checked={
-  //         table.getIsAllPageRowsSelected() ||
-  //         (table.getIsSomePageRowsSelected() && "indeterminate")
-  //       }
-  //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-  //       aria-label="Select all"
-  //       className=""
-  //     />
-  //   ),
-  //   cell: ({ row }) => (
-  //     <Checkbox
-  //       checked={row.getIsSelected()}
-  //       onCheckedChange={(value) => row.toggleSelected(!!value)}
-  //       aria-label="Select row"
-  //       className=" "
-  //     />
-  //   ),
-  //   enableSorting: false,
-  //   enableHiding: false,
-  // },
   {
     accessorKey: "id_user",
     header: ({ column }) => (
@@ -54,31 +86,30 @@ export const columns: ColumnDef<User>[] = [
     cell: ({ row }) => <TruncatedCell text={row.getValue("email")} />,
   },
   {
-    accessorKey: "password",
-    header: "Contraseña",
-    size: 30,
-    cell: ({ row }) => <PasswordCell password={row.getValue("password")} />,
-  },
-  {
-    accessorKey: "date",
-    header: "Fecha",
-    cell: formatDateCell("date"),
-  },
-  {
     accessorKey: "rol",
     header: "Rol",
-    size: 30,
+    size: 50,
+    cell: ({ row }) => (
+      <p className="text-center"> {row.getValue("rol")} </p>
+    ),
   },
   {
     accessorKey: "active",
     header: "Estado",
     size: 10,
     cell: ({ row }) => {
-      const value = row.getValue("active"); 
+      const value = row.getValue("active");
       return <p className="text-center">{value === 1 ? "Activo" : "No activo"}</p>;
     },
   },
-  
+  {
+    accessorKey: "codigo",
+    header: "Código",
+    size: 30,
+    cell: ({ row }) => (
+      <p className="text-center"> {row.getValue("codigo")} </p>
+    ),
+  },
   {
     accessorKey: "actions",
     header: "Actions",
@@ -86,3 +117,4 @@ export const columns: ColumnDef<User>[] = [
     cell: ({ row }) => <ActionUser row={row} />,
   },
 ];
+
