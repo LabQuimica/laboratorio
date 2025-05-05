57ddb09c5159a0139ccf63c6a78051ea52da93b0
DiegoCastr00 <diego.castro.elvira@gmail.com>
2025-05-05 16:29:06 -0600

    Update dependencies and enhance TableControls component
    - Upgraded `@tanstack/react-table` and `@tanstack/table-core` to version 8.21.3 for improved functionality and performance.
    - Refactored `TableControls` component to manage input state more effectively with local state and useEffect hooks, improving the responsiveness of the global filter input.
    - Simplified the query invalidation logic while ensuring error handling remains intact.

--- src/components/table/TableControls.tsx
+++ src/components/table/TableControls.tsx
@@ -4,6 +4,7 @@ import { Button } from "../ui/button";
 import { IconReload } from "@tabler/icons-react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useToast } from "@/hooks/use-toast";
+import { useState, useEffect } from "react";
 
 interface TableControlsProps {
   globalFilter: string;
@@ -25,18 +26,31 @@ export const TableControls = ({
   const queryClient = useQueryClient();
   const { toast } = useToast();
 
+  const [inputValue, setInputValue] = useState(globalFilter);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setGlobalFilter(inputValue);
+    }, 100);
+
+    return () => clearTimeout(timeout);
+  }, [inputValue, setGlobalFilter]);
+
+  useEffect(() => {
+    setInputValue(globalFilter);
+  }, [globalFilter]);
+
   const handleInvalidate = async (reactQueryKEYs: string[]) => {
     try {
       await queryClient.invalidateQueries({ queryKey: reactQueryKEYs });
 
-      // Espera a que las queries se revaliden y verifica si hay errores
       const queries = queryClient
         .getQueryCache()
         .findAll({ queryKey: reactQueryKEYs });
 
       for (const query of queries) {
         if (query.state.error) {
-          throw query.state.error; // Lanza el error si existe
+          throw query.state.error;
         }
       }
 
@@ -58,19 +72,16 @@ export const TableControls = ({
   return (
     <div className="flex items-center justify-between mb-4">
       <div className="flex items-center gap-2">
-        {/* Barra de búsqueda */}
         <Input
           placeholder="Buscar..."
-          value={globalFilter}
-          onChange={(e) => setGlobalFilter(e.target.value)}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
           className="max-w-sm border-gray-300 rounded-2xl"
         />
 
-        {/* Componente de filtros personalizado */}
         {filterComponent}
       </div>
       <div className="flex items-center gap-8">
-        {/* Información de filas mostradas */}
         <div className="text-sm text-gray-700 dark:text-white">
           Mostrando {filteredRowCount} de {totalRowCount} filas
         </div>
